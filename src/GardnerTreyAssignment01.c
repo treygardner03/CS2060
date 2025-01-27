43bb17cb59e4a20bd4d5579ad7ed653a9aa15570
Trey Gardner <treygardner72@gmail.com>
2025-01-27 16:06:27 -0700

    basic main constructions and assignment outline

---
+++ src/GardnerTreyAssignment01.c
@@ -0,0 +1,30 @@
+/*Name: Trey Gardner
+Class: CS2060 (MW)
+Due: 01-29-25
+Description: Uses hardcoded and user inputs to calculate the area of specified shapes
+and other general calculation
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159
+int main(argc, char** argv) {
+  //1.1 calculating the area of a rectangle
+    int rectangle_length;
+    int rectangle_width;
+    //Calculate area
+    double rectangle_area = rectangle_length * rectangle_width;
+    printf("The area of the rectangle is %lf\n", rectangle_area);
+
+  //1.2 Calculate the area of a circle and volume of a cylinder
+    int circle_radius;
+    double circle_area = PI * circle_radius * circle_radius;
+    printf("The area of the circle is %lf\n", circle_area);
+    
+  //1.3 Convert from Fahrenheit to Celcius
+
+  //1.4 Calculating simple interest
+
+
+  }
+
+
