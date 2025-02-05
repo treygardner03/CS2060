1823191ed926d5b0b056f62a7053bd4db64bbe75
Trey Gardner <treygardner72@gmail.com>
2025-02-05 12:57:15 -0700

    Comment structure to assignment

---
+++ src/GardnerTreyAssignment03.c
@@ -0,0 +1,43 @@
+/*Name: Trey Gardner
+Class: CS2060 (MW)
+Due: 02-17-25
+Description: This program will; 1. create a reverse order multiplication table of an input
+2. calculate the sum of the values in the multiplication table of #8 (1-10)
+3. calculate the factorial of any inputted number (whole number)
+4. determine if an inputted number is a prime number or not
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  //3.1 Print a multiplication talbe of an inputted value
+
+  //recieving input value
+
+  //calculating multiplication table
+
+  //printing table (reverse order)
+
+  //3.2 Summing the values of 8's multiplication table
+
+  //calculating 8's multiplication table
+
+  //summing and printing the values in 8's multiplication table
+
+  //3.3 Calculate the factorial of an inputted number (whole number)
+
+  //recieving input value
+
+  //calculate factorial
+
+  //print value
+
+  //3.4 Determining if an inputted value is a prime number or not
+
+  //recieving input value
+
+  //determine if prime
+
+  
+  }
