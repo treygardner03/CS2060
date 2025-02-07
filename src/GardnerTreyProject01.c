af3d9afdb70940d7b66c1c9c899ae4568caf2a0a
Trey Gardner <treygardner72@gmail.com>
2025-02-07 14:24:30 -0700

    base build of project 1

--- src/GardnerTreyProject01.c
+++ src/GardnerTreyProject01.c
@@ -7,7 +7,31 @@ either "Higher" or "Lower" until the user guesses the number
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
+ int maximum_value = 1000;
+ int minimum_value = 1;
+ int user_guess;
+  srand(time(NULL));
+ int random_number = rand() % maximum_value + minimum_value;
 
+ printf("Please try to guess a number betweeen 1-1000: ");
+
+ while (user_guess != random_number) {
+ scanf("%d", &user_guess);
+   if(user_guess < minimum_value) {
+     printf("Please enter a number betweeen 1-1000: ");
+   }
+   if(user_guess > maximum_value) {
+     printf("Please enter a number betweeen 1-1000: ");
+   }
+   if(user_guess < random_number) {
+     printf("Enter a higher number: ");
+   }
+   if(user_guess > random_number) {
+     printf("Enter a lower number: ");
+   }
+ }
+ printf("You Win!!");
 }
