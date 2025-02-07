ce4b145b59b5b07bdfb9db78e93d206726f70406
Trey Gardner <treygardner72@gmail.com>
2025-02-07 10:04:48 -0700

    basically done with assignment #3

--- src/GardnerTreyAssignment03.c
+++ src/GardnerTreyAssignment03.c
@@ -9,35 +9,73 @@ Description: This program will; 1. create a reverse order multiplication table o
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
-  //3.1 Print a multiplication talbe of an inputted value
+    //3.1 Print a multiplication table of an inputted value
+    int user_table_limit;
 
-  //recieving input value
+    //receiving input value
+    printf("Please input the size of the multiplication table: ");
+    scanf("%d", &user_table_limit);
 
-  //calculating multiplication table
+    //calculating multiplication table
+    for(int i = 1; i <= user_table_limit; i++) {
+        printf("%-5d", i);
+    }
+    for (int j = 2; j <= user_table_limit; j++) {
+        printf("\n");
+        for (int i = 1; i < user_table_limit + 1; i++) {
+            printf("%-5d", (i*j));
+        }
+    }
 
-  //printing table (reverse order)
+    //printing table (reverse order)
 
-  //3.2 Summing the values of 8's multiplication table
+    //3.2 Summing the values of 8's multiplication table
+    const int integer_for_table = 8;
+    const int table_minimum = 1;
+    const int table_maximum = 10;
+    int multiplication_table_value = 0;
+    int summation_value = 0;
 
-  //calculating 8's multiplication table
+    //calculating 8's multiplication table
+    for(int i = table_minimum; i < table_maximum + 1; i++) {
+        multiplication_table_value = integer_for_table * i;
+        summation_value += multiplication_table_value;
+    }
+    //printing the sum of the values in the multiplication table
+    printf("\n\nMultiplication table summation value: %d\n", summation_value);
 
-  //summing and printing the values in 8's multiplication table
+    //3.3 Calculate the factorial of an inputted number (whole number)
+    int user_factorial_value;
 
-  //3.3 Calculate the factorial of an inputted number (whole number)
+    //receiving input value
+    printf("\nPlease input the factorial base value: ");
+    scanf("%d", &user_factorial_value);
+    int factorial_value = user_factorial_value;
+    //calculate factorial
+    for(int i = user_factorial_value - 1; i > 0; i--) {
+        factorial_value = factorial_value * i;
+    }
+    //print value
+    printf("\nFactorial value: %d", factorial_value);
 
-  //recieving input value
+    //3.4 Determining if an inputted value is a prime number or not
+    int user_prime_number;
+    int prime_counter = 2;
+    bool is_prime = true;
 
-  //calculate factorial
+    //receiving input value
+    printf("\nPlease input the number in question: ");
+    scanf("%d", &user_prime_number);
 
-  //print value
-
-  //3.4 Determining if an inputted value is a prime number or not
-
-  //recieving input value
-
-  //determine if prime
-
-
-  }
+    //determine if prime
+    while (is_prime == true && prime_counter < user_prime_number) {
+        if (user_prime_number % prime_counter == 0 && prime_counter != user_prime_number) {
+            is_prime = false;
+        }
+        prime_counter++;
+    }
+    is_prime ? printf("\nYour number IS prime!") : printf("\nYour number is not prime!");
+}
---
+++ src/GardnerTreyProject01.c
@@ -0,0 +1,13 @@
+/*Name: Trey Gardner
+Class: CS2060 (M/W)
+Due: 02-19-25
+Description: This program will; Generate a random number, then respond to user input with
+either "Higher" or "Lower" until the user guesses the number
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+}
