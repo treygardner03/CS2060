e325c893db80b7fbedf6d8032260266acf3988a4
Trey Gardner <treygardner72@gmail.com>
2025-02-26 14:13:29 -0700

    starting assignment 5

---
+++ src/GardnerTreyAssignment05.c
@@ -0,0 +1,23 @@
+/*Name: Trey Gardner
+Class: CS2060 (M/W)
+Due: February 26th, 2025
+Description: This program will;
+1.) Takes a 3x3 matrix input and returns the largest value in each row
+2.) Takes a 3x3 matrix and sums the diagonal values
+3.) Checks for symetry in a 3x3 matrix
+4.) Takes a 3x3 matrix input and replaces all negative values with 0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS = 3;
+#define COLS = 3;
+int main() {
+  //5.1 - Find the largest value in each row
+ int user_matrix[ROWS][COLS];
+  //5.2 - Sum the diagonal values
+
+  //5.3 - Check for symmetry in matrix
+
+  //5.4 - Replace all negatives with 0
+
+ }
