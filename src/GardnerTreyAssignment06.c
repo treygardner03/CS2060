3ce357ac8073ea077efd714c2ff353f0033ce637
Trey Gardner <treygardner72@gmail.com>
2025-03-09 10:38:13 -0600

    starting assignment

---
+++ src/GardnerTreyAssignment06.c
@@ -0,0 +1,21 @@
+/*Name: Trey Gardner
+Class: CS2060 (M/W)
+Due: March 10th, 2025
+Description: This program will;
+6.1 - copy one string into another without using built in functions
+6.2 - checks whether a given string is a palindrome or not
+6.3 - counts the number of vowels and consonants in a giveen string
+6.4 - finds the longest word in a given string (returns the first if there are multiple)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+//6.1 copying one string to another (manually)
+  //A.) take a string as an input
+  char input_string[100];
+  printf("\nEnter a string to be copied: ");
+  scanf("%s", input_string);
+}
