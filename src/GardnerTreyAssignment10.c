68128ebdeab407254aa577b451f5e57f8fb5e843
Trey Gardner <treygardner72@gmail.com>
2025-04-09 18:36:57 -0600

    finishing assignment 10?

--- src/GardnerTreyAssignment10.c
+++ src/GardnerTreyAssignment10.c
@@ -9,6 +9,7 @@ Description: This program will;
 10.4: recursively add the digits of an integer until the result is a single digit
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #define LINE_BREAK "\n-------------------------------------------------------------------------\n"
 
@@ -17,8 +18,8 @@ Description: This program will;
 
 //function prototypes
 int greatest_common_divisor(int a, int b);
-int recursive_summation(int int_array[]);
-int recursive_vowel_count(char string[]);
+int recursive_summation(int int_array[], int size);
+int recursive_vowel_count(char *string);
 int recursive_integer_reduction(int num);
 
 int main()
@@ -34,6 +35,7 @@ int main()
   scanf("%d", &first_int);
   printf("\nPlease enter your second integer: ");
   scanf("%d", &second_int);
+  getchar();
 
   //finding GCD
   int gcd = greatest_common_divisor(first_int, second_int);
@@ -45,6 +47,7 @@ int main()
 //10.2 - recursive summation
   //predefined array
   int int_array[] = {2, 3, 6, 3, 6, 1, 56, 74, 24, 30, 16};
+  int size = sizeof(int_array) / sizeof(int);
 
   //prompting user
   printf("%sWe are now going to find the sum of an integer array using a recursive algorithm!%s", LINE_BREAK, LINE_BREAK);
@@ -56,10 +59,10 @@ int main()
   }
 
   //summing array
-  int recursive_sum = recursive_summation(int_array);
+  int recursive_sum = recursive_summation(int_array, size);
 
   //displaying results
-  printf("The sum of the array above is: %d\n", recursive_sum);
+  printf("\nThe sum of the array above is: %d\n", recursive_sum);
 
 //10.3 - recursive vowel count
   //user defined string
@@ -68,7 +71,8 @@ int main()
   //prompting user
   printf("%sWe are now going to count the vowels in a string using recursion!%s", LINE_BREAK, LINE_BREAK);
   printf("\nPlease enter your string: ");
-  scanf("%s", string);
+  scanf("%[^\n]s", string);
+  getchar();
 
   //counting vowels
   int total_vowels = recursive_vowel_count(string);
@@ -97,23 +101,44 @@ int main()
 //finding the greatest common devisor
 int greatest_common_divisor(int a, int b)
 {
-
-}
+  if(b == 0)
+  {
+    return a;
+  }//if
+     return greatest_common_divisor(b, a % b);
+}//greatest_common_devisor
 
 //summing integer array
-int recursive_summation(int int_array[])
+int recursive_summation(int int_array[], int size)
 {
+  if (size == 0)
+  {
+    return 0;
+  }
+  return recursive_summation(int_array, size -1) + int_array[size - 1];
 
 }
 
 //counting vowels in a string
-int recursive_vowel_count(char string[])
+int recursive_vowel_count(char *string)
 {
+  if (*string == '\0')
+  {
+    return 0;
+  }
+  char c = tolower(*string);
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'u' || c == 'o' || c == 'y')
+  {
+    return 1 + recursive_vowel_count(string + 1);
+  }
+  return recursive_vowel_count(string + 1);
 
 }
 
 //reducing integer using addition
 int recursive_integer_reduction(int num)
 {
-
+  if (num < 10) return num;
+  int sum = recursive_integer_reduction(num / 10) + recursive_integer_reduction(num % 10);
+  return recursive_integer_reduction(sum);
 }
