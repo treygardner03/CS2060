4d37dc19e9991363b6791535ea20794c66f9d39c
Trey Gardner <treygardner72@gmail.com>
2025-04-14 09:46:16 -0600

    assignment 11 outline

---
+++ src/GardnerTreyAssignment11.c
@@ -0,0 +1,25 @@
+/*Name: Trey Gardner
+Class: CS2060 (M/W)
+Due: 04-14-25
+Assignment: 11
+Description: This program will;
+11.1 - Create struct "Student" then create and display and array of "Student"'s
+11.2 - Create struct "Employee" then create and array and display "Employee" with highest and lowest
+salary using both direct access and pointer arithmetic.
+11.3 - Create struct "Product" then create and array user pointers to display all "Products", display
+highest stock value, and allow the user to update the quantity of any product by ProductID
+11.4 - Create two structs with the same members in different orders, and demonstrate how the order
+of members in the definition may affect the memory layout due to "padding". Then disable padding for the
+two structs and re-display their memory sizes.
+ */
+
+//libraries
+#include <stdio.h>
+
+//prototypes
+
+//main
+int main(void)
+{
+
+}
