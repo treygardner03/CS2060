6dda065813f3ae6984df53ad2e4a4db2a3f73d2f
Trey Gardner <treygardner72@gmail.com>
2025-04-20 17:31:40 -0600

    finished assignment 12
    finished assignment 12

--- src/GardnerTreyAssignment12.c
+++ src/GardnerTreyAssignment12.c
@@ -15,6 +15,7 @@ Description: This program will;
 #include <ctype.h>
 #include <stdio.h>
 #define NUM_OF_STUDENTS 3
+#define LINE_BREAK "\n---------------------------------------\n"
 
 //prototypes\Structs
 //Student struct
@@ -30,6 +31,8 @@ struct Student
 int main(void)
 {
 //12.1 - Storing student  data
+    //Operation Prompt
+    printf("\nCreating .txt file and writing student records: %s", LINE_BREAK);
     //creating student records
     struct Student Trey = {111466749, "Trey Gardner", 3.46f},//Student 1
                    Haylie = {734777347, "Haylie Gardner", 4.00f},//Student 2
@@ -45,7 +48,7 @@ int main(void)
     fprintf(student_records, "All Student Records: \n");
     for (int i = 0; i < NUM_OF_STUDENTS; i++)
     {
-        fprintf(student_records, "Student #%i -> Name: %s GPA: %.2f\n", all_students[i].student_id, all_students[i].name, all_students[i].gpa_score);
+        fprintf(student_records, "-> Student #%i -> Name: %s GPA: %.2f\n", all_students[i].student_id, all_students[i].name, all_students[i].gpa_score);
     }
 
     //display contents of file
@@ -63,61 +66,141 @@ int main(void)
     fclose(student_records);
 
 //12.2 - Doubling integer from file
+
+    //Operation Prompt
+    printf("\nDoubling Integer value read from text file: %s", LINE_BREAK);
+
     //creating file with integer value
     FILE *integers = fopen("integers.txt", "w+");
     fprintf(integers, "%d", 43);
 
-    //reading and doubling value
+    //reading integer from text file
     rewind(integers);
     int unmodified_value;
     fscanf(integers, "%d", &unmodified_value);
+
+    //printing current integer value
+    printf("-> Current Integer in .txt file: %d\n", unmodified_value);
+
+    //doubling integer value
     int modified_value = unmodified_value * 2;
     rewind(integers);
     fprintf(integers, "%d", modified_value);
+
+    //printing new integer:
+    printf("-> New Integer in .txt file: %d\n", modified_value);
+
     //closing file
     fclose(integers);
 
 //12.3 - Modifying text file
+    //Operation Prompt
+    printf("\nCreating a text file, then modifying the contents to: "
+           "\n1.) covert all characters to uppercase"
+           "\n2.) prompt the user to remove a line by line number %s", LINE_BREAK);
+
     //creating text file
     FILE *text_file = fopen("text.txt", "w+");
-    fprintf(text_file, "%s", "Hello my name is trey and this is my\ntest file with some WonKY choices to make\nsurethat everytinbg is working :)");
+    fprintf(text_file, "%s", "hello my name is trey and this is my\ntest file with some WonKY choices to make"
+                             "\nsurethat everytinbg is working :)");
     rewind(text_file);
 
-    //converting to upper case
-    char initial_text[100];
-    fgets(initial_text, EOF, text_file);
-    (initial_text);
-    //to upper case
-    rewind(text_file);
-    fprintf(text_file, "%s", initial_text);
+//1.) CHANGING TEXT TO UPPER CASE
+
+    //creating string with length of text file
+    fseek(text_file, 1, SEEK_END);
+    long length = ftell(text_file);
+    char file_to_string[length +1];
     rewind(text_file);
 
-    //prompting for line to remove
-    printf("Enter the line # of the line you would like to delete from the file: ");
-    int line_number = 0;
-    scanf("%d", line_number);
-
-    //Removing specified line
-    FILE *final_text = fopen("final.txt", "w+");
-    while (text_file != EOF)
+    //writing the contents of the text file to the string
+    for (int i = 0; i < length; i++)
     {
-        int counter = 0;
-        if (counter != line_number)
-        {
-            char print_string[100];
-            fgets(print_string, '\n', initial_text);
-            fprintf(final_text, "%s", print_string);
-        }
-        counter++;
+        file_to_string[i] = fgetc(text_file);
+    }
+    file_to_string[length] = '\0';//adding null terminator
+
+    //printing current text file contents
+    printf("-> Current text files contents:\n%s\n", file_to_string);
+
+    //converting to uppercase
+    for (int i = 0; i < length; i++)
+    {
+        file_to_string[i] = toupper(file_to_string[i]);
     }
 
-    //creating new file with modified contents
+    //printing new file contents
+    printf("\n-> New text file contents:\n%s\n", file_to_string);
+
+    //overwriting new text to file
+    rewind(text_file);
+    fprintf(text_file, "%s", file_to_string);
+    rewind(text_file);
+
+//2.) REMOVING LINE BY LINE NUMBER
+
+    //prompting for line to remove
+    int line_number = 0;
+    printf("\nEnter the line number of the line you would like to delete from the file:%sLine Number: ", LINE_BREAK);
+    scanf("%d", &line_number);
+
+    //creating file for text without specified line
+    char current_char;
+    int char_counter = 0;
+    int current_line_number = 0;
+    FILE *final_text = fopen("final.txt", "w+");
+
+    //coping text from initial to final excluding the specified line
+    for (int i = 0; i < length; i++)
+    {
+        current_char = fgetc(text_file);
+
+       if (current_char == '\n')
+       {
+           current_line_number++;
+       }
+
+        if (current_line_number == line_number)
+        {
+            while (*text_file->_ptr != '\n')
+            {
+                text_file->_ptr++;
+            }
+            current_line_number++;
+        }
+        else
+        {
+            file_to_string[char_counter] = current_char;
+            char_counter++;
+        }
+    }//for
+    rewind(text_file);
+    file_to_string[char_counter + 1] = '\0';
+
+    //printing final text file
+    fprintf(final_text, "%s", file_to_string);
+    printf("-> Your final text file omitting the specified line is: \n%s\n", file_to_string);
+
+    //closing file
+    fclose(final_text);
 
 //12.4 - displaying the size of a given file in bytes
     //checking that file exists
-
-    //calculating size of file
+    FILE* test_file = fopen("Dookie.txt", "r");
+    if (test_file != NULL)
+    {
+        //calculating length of file
+        fseek(test_file, 1, SEEK_END);
+        length = ftell(test_file);
 
     //display size of file in bytes
+    printf("The size of the file above in bytes is %d", length);
+
+    }//checking if file exists
+    else
+        {
+            printf("File does not Exist");
+        }
+
 
 }//main
